83e3c4ae05f3b3cb76e0484e3fc6cf8c51584174
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-05-28 15:37:39 -0300

    [logs] add log

--- src/modules/stock/services/stock.service.ts
+++ src/modules/stock/services/stock.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { OrderEventDTO } from '../dto/update-stock-from-order-event.dto';
 import { StockRepository } from '../repositories/stock.repository';
 
@@ -9,6 +9,7 @@ export class StockService {
   async updateProductsStockFromOrderEvent(
     orderEventDto: OrderEventDTO,
   ): Promise<void> {
+    Logger.log({ message: 'Updating stocks from order', orderEventDto });
     return this.stockRepository.subtractStockInBatch(orderEventDto.items);
   }
 }
